cf90bbf43b1b175fe4a72087c1c51e52b109389b
Lydia Gray <lydia.gray@stockport.gov.uk>
2019-01-04 14:39:57 +0000

    Added db type file filter to JFileChooser

--- HousePriceTrends/src/Controller.java
+++ HousePriceTrends/src/Controller.java
@@ -2,10 +2,13 @@ import java.io.File;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Controller {	
 	public static void main(String[] args) {
 		JFileChooser jfc = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("Databases", "db");
+		jfc.setFileFilter(filter);
 		
 		int result = jfc.showOpenDialog(new JFrame());
 		if (result == JFileChooser.APPROVE_OPTION) {
